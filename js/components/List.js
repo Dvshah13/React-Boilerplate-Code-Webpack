da8d08a5236d22ff317b66c72946ba63ef2c772e
Dvshah13 <dvshah13@gmail.com>
2017-03-06 16:42:28 -0600

    React slack app

--- js/components/List.js
+++ js/components/List.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+OAuth Token: xoxp-149583067808-149583068192-150293942834-4aeceb0be75edb7ded1a881ddb3bedaa
+
 export default class List extends React.Component {
     render() {
         return (
@@ -11,5 +13,17 @@ export default class List extends React.Component {
         );
     }
 }
+
+// Show channel listing
+axios.post('https://slack.com/api/channels.list?token=xoxp-149583067808-149583068192-150342085556-c7b575098803fc466a8d9905b4a4ace0&pretty=1')
+
+// Load a channel messages
+axios.post('https://slack.com/api/channels.history?token=xoxp-149583067808-149583068192-150342085556-c7b575098803fc466a8d9905b4a4ace0&channel=C4EB439J9&pretty=1')
+
+// Add methods
+axios.post('https://slack.com/api/chat.postMessage?token=xoxp-149583067808-149583068192-150342085556-c7b575098803fc466a8d9905b4a4ace0&channel=C4EB439J9&text=Hola&pretty=1')
+
+// Delete method
+axios.post('https://slack.com/api/chat.delete?token=xoxp-149583067808-149583068192-150342085556-c7b575098803fc466a8d9905b4a4ace0&ts=1488820611.930208&channel=C4EB439J9&pretty=1')
 
 // module.exports = List;
--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import List from "./components/List";
+import LoadMessages from "./components/LoadMessages";
+import ChannelList from "./components/ChannelList";
+import AddMessages from "./components/AddMessages";
+import axios from "axios";
+
 console.log("working");
 
+
+
 class Demo extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            message: " ",
+            timestamp: Date.now()
+        }
+        this.doMessage = this.doMessage.bind(this);
+    }
+
+    doMessage(message,event){
+        this.setState({message:message});
+        var self = this;
+        axios.post(`https://slack.com/api/chat.postMessage?token=xoxp-149583067808-149583068192-150342085556-c7b575098803fc466a8d9905b4a4ace0&channel=C4EB439J9&text=${message}&pretty=1.json`)
+            .then(function(){
+                self.setState({
+                    timestamp: Date.now(),
+                    message: message
+                });
+            });
+    }
+
         render() {
             return (
                 <div>
                     <div>
-                    I am a react Component
+                    My Channels
+                    <ChannelList />
+                    My messages
+                    <LoadMessages />
+                    <AddMessages doMessage={this.doMessage} />
                     </div>
-                    <List />
+
                 </div>
             );
         }
